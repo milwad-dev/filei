cf2240c9a261f995eaa13c275972d21238137e21
Milwad <milwad.dev@gmail.com>
2024-09-23 19:48:29 +0330

    add `UploadFile`

--- filei.go
+++ filei.go
@@ -1,1 +1,23 @@
 package filei
+
+import (
+	"io"
+	"mime/multipart"
+	"os"
+)
+
+// UploadFile saves an uploaded file to the specified path.
+func UploadFile(file multipart.File, fileName string) error {
+	out, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, file)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
