2a2ca7e8ec5262f6a30bf43f0fb32f4aa70c9c00
Milwad <milwad.dev@gmail.com>
2024-09-25 12:32:19 +0330

    add `MoveFile`

--- filei.go
+++ filei.go
@@ -1,6 +1,7 @@
 package filei
 
 import (
+	"fmt"
 	"io"
 	"mime/multipart"
 	"os"
@@ -89,5 +90,33 @@ func DeleteDirectory(directory string) error {
 		return err
 	}
 
+	return nil
+}
+
+// MoveFile move the file from one place to another place.
+func MoveFile(sourcePath, destPath string) error {
+	inputFile, err := os.Open(sourcePath)
+	if err != nil {
+		return fmt.Errorf("couldn't open source file: %v", err)
+	}
+	defer inputFile.Close()
+
+	outputFile, err := os.Create(destPath)
+	if err != nil {
+		return fmt.Errorf("couldn't open dest file: %v", err)
+	}
+	defer outputFile.Close()
+
+	_, err = io.Copy(outputFile, inputFile)
+	if err != nil {
+		return fmt.Errorf("couldn't copy to dest from source: %v", err)
+	}
+
+	inputFile.Close()
+
+	err = os.Remove(sourcePath)
+	if err != nil {
+		return fmt.Errorf("couldn't remove source file: %v", err)
+	}
 	return nil
 }
